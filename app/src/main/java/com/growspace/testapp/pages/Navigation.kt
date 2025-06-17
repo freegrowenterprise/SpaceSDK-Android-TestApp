40a192637e1120dff9a87e14a8166753aab55cde
Mingwan Choi <wbsldj7645@naver.com>
2025-06-17 15:11:17 +0900

    feat add azimuth, elevation check page (#7)

--- app/src/main/java/com/growspace/testapp/pages/Navigation.kt
+++ app/src/main/java/com/growspace/testapp/pages/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.growspace.testapp.pages.check_uwb.CheckAzimuthElevationPage
 import com.growspace.testapp.pages.rtls.DeviceCoordinateViewModel
 import com.growspace.testapp.pages.rtls.RTLSPage
 
@@ -20,5 +21,6 @@ fun AppNavHost() {
         composable("ranging") { RangingPage() }
         composable("rtls") { RTLSPage(navController, viewModel = viewModel) }
         composable("uwbSetting") { UwbSettingPage(viewModel = viewModel) }
+        composable("check") { CheckAzimuthElevationPage() }
     }
 }
